133a57a9960e7998e1e4561b1d2def44441986dc
Kayode <babalolakayode44@gmail.com>
2023-06-14 16:02:33 +0100

    minor sidebar changes

--- src/components/UI/SideBar.tsx
+++ src/components/UI/SideBar.tsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 const SideBar = (props:any) =>{
 
     return (
-      <div className="p-6 min-h-full top-0 left-0 fixed bg-green-800 flex flex-col scroll min-w-full z-10 md:hidden">
+      <div className="p-6 min-h-full top-0 left-0 fixed bg-green-800 text-black flex flex-col justify-around scroll min-w-full z-10 md:hidden">
         <button
           className="h-14 w-14 rounded self-end"
           type="button"
@@ -12,24 +12,27 @@ const SideBar = (props:any) =>{
         >
           <span className="material-symbols-outlined">close</span>
         </button>
-        <ul className="bg-inherit w-full flex flex-col gap-9 place-items-center justify-center items-center min-h-screen">
+        <ul className="text-2xl bg-inherit w-full flex flex-col gap-9 place-items-center justify-start pt-20 items-center min-h-screen">
           <li>
             <button onClick={props.onCloseClick}>
               <Link to="/">Home</Link>
             </button>
           </li>
+          <hr />
 
           <li>
             <button onClick={props.onCloseClick}>
               <Link to="/rando">Resume</Link>
             </button>
           </li>
+          <hr />
 
           <li>
             <button onClick={props.onCloseClick}>
               <Link to="/">Articles</Link>
             </button>
           </li>
+          <hr />
 
           <li>
             <button onClick={props.onCloseClick}>
